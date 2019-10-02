d8fd0c18cb4d3840f15f5f81adf606c0cd052c76
jadna <jadna.ac@gmail.com>
2019-10-01 23:30:55 -0300

    fazendo a função que calcula as distancias

--- www/index.php
+++ www/index.php
@@ -12,15 +12,47 @@ d($servicie->showAllPlaces());
 $auxs_group = $servicie->showAllGroups();
 $auxs_poi = $servicie->showAllPlaces();
 
-foreach($auxs_group as $value){
-    $result_group['latitude'] = $value->latitude;
-    $result_group['longitude'] = $value->longitude;    
+// Função que converte para um array a latitude e longitude
+function getPoints($array){
+
+    foreach($array as $key => $value){
+        $result[$key]['latitude'] = $value->latitude;
+        $result[$key]['longitude'] = $value->longitude;    
+    }
+
+    return $result;
 }
 
-foreach($auxs_poi as $value){
-    $result_poi['latitude'] = $value->latitude;
-    $result_poi['longitude'] = $value->longitude;    
+/** Função que calcula a distância do ponto de encontro do grupo para os pontos de interesse */
+function calculateDistance($auxs_group, $auxs_poi){
+
+    $points_group = getPoints($auxs_group);
+    $points_poi = getPoints($auxs_poi);
+
+    //print_r("<pre>".print_r($points_poi,true)."</pre>");
+
+    foreach($points_poi as $key => $value){
+
+        $teste[$key] = distancia(floatval($points_group[0]['latitude']), floatval( $points_group[0]['longitude']), floatval($points_poi[$key]['latitude']), floatval($points_poi[$key]['longitude']));
+        
+    }
 }
+print_r("<pre>".print_r($teste,true)."</pre>");
+
+function distancia($lat1, $lon1, $lat2, $lon2) {
+
+    $lat1 = deg2rad($lat1);
+    $lat2 = deg2rad($lat2);
+    $lon1 = deg2rad($lon1);
+    $lon2 = deg2rad($lon2);
+    
+    $dist = (6371 * acos( cos( $lat1 ) * cos( $lat2 ) * cos( $lon2 - $lon1 ) + sin( $lat1 ) * sin($lat2) ) );
+    $dist = number_format($dist, 2, '.', '');
+    print_r("<pre>".print_r($dist,true)."</pre>");
+    return $dist;
+}
+    
+   // echo distancia(-12.9813346,-38.4653612, -12.9741491,-38.4696483) . " Km<br />";
 
 
 ?>
