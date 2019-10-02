d6187b76a4c19f6553bec6c285e57ab2eac9aa42
DPE <dpe@defensoria.ba.def.br>
2019-10-01 23:08:54 -0300

    change pdo obj to pdo array

--- www/index.php
+++ www/index.php
@@ -6,11 +6,13 @@ require 'services/RecommendationSystemService.class.php';
 $servicie = new RecommendationSystemService();
 
 /* Get all places on database */
-d($servicie->showAllGroups());
-d($servicie->showAllPlaces());
+//d($servicie->showAllGroups());
+//d($servicie->showAllPlaces());
 
 $auxs_group = $servicie->showAllGroups();
 $auxs_poi = $servicie->showAllPlaces();
+
+//d($auxs_group);
 
 foreach($auxs_group as $value){
     $result_group['latitude'] = $value->latitude;
@@ -18,6 +20,10 @@ foreach($auxs_group as $value){
 }
 
 foreach($auxs_poi as $value){
+
+    echo $value->latitude;
+    echo $value->longitude;   
+
     $result_poi['latitude'] = $value->latitude;
     $result_poi['longitude'] = $value->longitude;    
 }
